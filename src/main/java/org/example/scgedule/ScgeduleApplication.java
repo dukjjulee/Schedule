7b7a266bb50b078730aecb21e1089bc9a297aedf
dukjjulee <nancy0523@naver.com>
2025-08-04 01:30:27 +0900

    docs: README.md

---
+++ src/main/java/org/example/scgedule/ScgeduleApplication.java
@@ -0,0 +1,13 @@
+package org.example.scgedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgeduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScgeduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/scgedule/ScgeduleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scgedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgeduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
